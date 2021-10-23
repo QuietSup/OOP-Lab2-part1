41d0e955e771eb1135b382dc39f134c2efb61b82
QuietSup <90607654+QuietSup@users.noreply.github.com>
2021-10-23 15:38:13 +0300

    Update 2p1-1.py

--- 2p1-1.py
+++ 2p1-1.py
@@ -1,44 +1,63 @@
 class Rectangle:
-    def __init__(self):
-        self.length = 1
-        self.width = 1
+    def __init__(self, width=None, length=None):
+        if width is not None:
+            if width <= 0 or width >= 20:
+                raise ValueError("Width is out of range (0; 20)")
+            if not isinstance(width, float):
+                raise ValueError("Width is not float")
+            self.__width = width
+        else:
+            self.__width = 1.0
 
-    def set(self, width, length):
+        if width is not None:
+            if length <= 0 or length >= 20:
+                raise ValueError("Length is out of range (0; 20)")
+            if not isinstance(length, float):
+                raise ValueError("Length is not float")
+            self.__length = length
+        else:
+            self.__length = 1.0
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def length(self):
+        return self.__length
+
+    @width.setter
+    def width(self, width):
         if width <= 0 or width >= 20:
-            self.width = "Width is out of range (0; 20)"
-        else:
-            if isinstance(width, float):
-                self.width = width
-            else:
-                self.width = "Width is not float"
+            raise ValueError("Width is out of range (0; 20)")
+        if not isinstance(width, float):
+            raise ValueError("Width is not float")
+        self.__width = width
+
+    @length.setter
+    def length(self, length=None):
+        if length is None:
+            return
         if length <= 0 or length >= 20:
-            self.length = "Length is out of range (0; 20)"
-        else:
-            if isinstance(length, float):
-                self.length = length
-            else:
-                self.length = "Length is not float"
+            raise ValueError("Length is out of range (0; 20)")
+        if not isinstance(length, float):
+            raise ValueError("Length is not float")
+        self.__length = length
 
-    def get_width(self):
-        return self.width
-
-    def get_length(self):
-        return self.length
-
+    @property
     def area(self):
-        return self.length * self.width
+        return self.__length * self.__width
 
+    @property
     def perimeter(self):
-        return 2 * (self.length + self.width)
+        return 2 * (self.__length + self.__width)
 
 
 a = Rectangle()
-a.set(1.0, 6.0)
-print("width:", a.get_width())
-print("length:", a.get_length())
+a.width = 9.5
+a.length = 1.2
+print("width:", a.width)
+print("length:", a.length)
+print("area:", a.area)
+print("perimeter:", a.perimeter)
 
-try:
-    print("area:", a.area())
-    print("perimeter:", a.perimeter())
-except:
-    print("Wrong data")
