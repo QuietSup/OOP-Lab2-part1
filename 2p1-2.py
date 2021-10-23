22be0c0986a4584ef9e5a14e3096762154487f34
QuietSup <90607654+QuietSup@users.noreply.github.com>
2021-10-23 15:38:57 +0300

    Update 2p1-2.py

--- 2p1-2.py
+++ 2p1-2.py
@@ -2,21 +2,32 @@ import math
 
 
 class Rational:
-    def __init__(self, numerator=1, denominator=1):
-        assert isinstance(numerator, int)
-        assert isinstance(denominator, int)
-        assert denominator != 0
+    def __init__(self, numerator=None, denominator=None):
+        if numerator is None:
+            numerator = 1
+        else:
+            if not isinstance(numerator, int):
+                raise ValueError("Numerator isn't int")
+        if denominator is None:
+            denominator = 1
+        else:
+            if not isinstance(denominator, int):
+                raise ValueError("Denominator isn't int")
+            if denominator == 0:
+                raise ValueError("Denominator can't be zero")
         divisor = math.gcd(numerator, denominator)
         self.__num = numerator // divisor
         self.__den = denominator // divisor
 
+    @property
     def show(self):
-        return (str(self.__num), '/', str(self.__div))
+        return f'{self.__num}/{self.__den}'
 
+    @property
     def show_float(self):
-        return self.__num / self.__div
+        return self.__num / self.__den
 
 
-a = Rational(2, 4)
-print(a.show())
-print(a.show_float())
+a = Rational()
+print(a.show)
+print(a.show_float)
